030a8e37726f86092d3d9c723c11df6d7123a9e3
dpetrov <dpetrov04111999@gmail.com>
2023-02-16 05:51:12 +0200

    finish

--- js-basic/java.js
+++ js-basic/java.js
@@ -1,1 +1,7 @@
+var p = 3.14;
+var numarH = prompt('Introduceți înălțimea');
+var numarR = prompt('Introduceți raza');
 
+var numarR2 = parseInt(numarR) * parseInt(numarR);
+var vol = p * numarH * numarR2;
+alert("Volumul cilindrului este: " + vol)
